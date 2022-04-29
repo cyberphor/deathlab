aad63bc8dbb10e7cdb69d261cf23a52b021df26d
Victor Fernandez III <48382964+cyberphor@users.noreply.github.com>
2022-04-28 21:04:16 -0700

    Update New-AdDomainAdmin.ps1

--- VirtualBox/Provisioning-Scripts/Domain-Controller/New-AdDomainAdmin.ps1
+++ VirtualBox/Provisioning-Scripts/Domain-Controller/New-AdDomainAdmin.ps1
@@ -4,6 +4,10 @@ $Parameters = @{
     Name                    = "Alderson, Elliot" 
     SamAccountName          = "elliot.alderson.da" 
     UserPrincipalName       = "elliot.alderson.da@evil.corp" 
+    Company                 = "Evil Corp"
+    Organization            = "Allsafe Security"
+    Description             = "Senior Cybersecurity Engineer"
+    EmployeeId              = "1337"
     AccountPassword         = $(ConvertTo-SecureString -AsPlainText -Force "1qaz2wsx!QAZ@WSX")
     ChangePasswordAtLogon   = $true 
     Enabled                 = $true
@@ -11,5 +15,4 @@ $Parameters = @{
 
 (Get-Service "ADWS").WaitForStatus("Running")
 New-ADUser @Parameters
-Add-ADGroupMember -Identity "Domain Admins" -Members "elliot.alderson.da"
-Enable-ADAccount -Identity "elliot.alderson.da"
+Add-ADGroupMember -Identity "Domain Admins" -Members $Parameters["SamAccountName"]
