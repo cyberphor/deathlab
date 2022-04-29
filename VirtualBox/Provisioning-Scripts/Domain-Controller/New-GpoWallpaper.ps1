a3af4640011589e5cb5fe3b673a52a387342ac8d
Victor Fernandez III <48382964+cyberphor@users.noreply.github.com>
2022-04-29 16:20:55 -0700

    Added more content

--- VirtualBox/Provisioning-Scripts/Domain-Controller/New-GpoWallpaper.ps1
+++ VirtualBox/Provisioning-Scripts/Domain-Controller/New-GpoWallpaper.ps1
@@ -3,21 +3,15 @@ $Policy = "Wallpaper"
 $Comment = "Sets the wallpaper to the company logo."
 New-GPO -Name $Policy -Comment $Comment
 
-# create a folder for the wallpaper
-$Share = "C:\$Policy"
-New-Item -ItemType Directory -Path $Share
-
-# copy the wallpaper from the Vagrant synced folder to the guest
-$Wallpaper = "evil-corp-wallpaper.jpg"
-Copy-Item "C:\Vagrant\$Wallpaper" $Share
-
 # share the folder containing the wallpaper
-New-SmbShare -Name $Policy -Path $Share -FullAccess "Administrators" -ReadAccess "Domain Users"
+$Wallpaper = "$Policy\evil-corp-wallpaper.jpg"
+$Path = "C:\" + $Wallpaper
+New-SmbShare -Name $Policy -Path $Path -FullAccess "Administrators" -ReadAccess "Domain Users"
 
 # set the wallpaper using the share as the path
 $Key = "HKCU\Software\Microsoft\Windows\CurrentVersion\Policies\System"
 $ValueName = "Wallpaper"
-$Value = '\\' + $env:COMPUTERNAME + '\' + $Policy + '\' + $Wallpaper
+$Value = '\\' + $env:COMPUTERNAME + '\' + $Wallpaper
 $Type = "String"
 Set-GPRegistryValue -Name $Policy -Key $Key -ValueName $ValueName -Value $Value -Type $Type
 
--- VirtualBox/Provisioning-Scripts/Domain-Controller/Wait-ForAdwsServiceToStart.ps1
+++ VirtualBox/Provisioning-Scripts/Domain-Controller/Wait-ForAdwsServiceToStart.ps1
@@ -1,8 +1,8 @@
-$Retries = 10
+$Retries = 6
 do {
     Write-Output "Waiting for Active Directory Web Services to start..."
     $AdwsServiceHasStarted = (Get-Service "ADWS").Status -eq "Running"
     $AdwsServiceHasStarted
     $Retries--
-    Start-Sleep -Seconds 60
+    Start-Sleep -Seconds 30
 } until ($AdwsServiceHasStarted -or $Retries -eq 0)
