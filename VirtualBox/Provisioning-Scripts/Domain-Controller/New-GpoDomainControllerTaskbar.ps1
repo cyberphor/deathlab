87ad2e79177e80dd6c52010e9d82c5291d18df96
Victor Fernandez III <48382964+cyberphor@users.noreply.github.com>
2022-05-01 22:10:38 -0700

    Added way more content

--- VirtualBox/Provisioning-Scripts/Domain-Controller/New-GpoDomainControllerTaskbar.ps1
+++ VirtualBox/Provisioning-Scripts/Domain-Controller/New-GpoDomainControllerTaskbar.ps1
@@ -2,7 +2,7 @@
 Import-GPO -BackupGpoName "Taskbar" -Path "C:\Vagrant\Group-Policy-Objects\Taskbar" -TargetName "Taskbar" -CreateIfNeeded
 
 # copy XML file
-Copy-Item -Path "C:\Vagrant\Group-Policy-Objects\Taskbar\Taskbar.xml" -Destination "C:\Windows\SYSVOL\Domain\Scripts\Taskbar.xml"
+Copy-Item -Path "C:\Vagrant\Configuration-Files\Taskbar.xml" -Destination "C:\Share\Taskbar.xml"
 
 # link the GPO
 $Target = 'ou=Domain Controllers,' + $(Get-ADDomain -Current LocalComputer).DistinguishedName
