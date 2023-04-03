bde4a4586e19fd374df5ce3bbbaa3c4465ae4650
Victor Fernandez III <48382964+cyberphor@users.noreply.github.com>
2023-04-03 05:59:59 -0700

    Reorganized the file structure

--- Labs/Detection-Engineering/Provisioning-Scripts/Join-Domain.ps1
+++
@@ -1,4 +0,0 @@
-$Username = "evil.corp\vagrant"
-$Password = ConvertTo-SecureString "vagrant" -AsPlainText -Force
-$Credential = New-Object System.Management.Automation.PSCredential $Username, $Password
-Add-Computer -DomainName "evil.corp" -Credential $Credential
--- Labs/Detection-Engineering/Provisioning-Scripts/Set-DnsServer.ps1
+++
@@ -1,3 +0,0 @@
-$IpAddress = "192.168.5.*"
-$InterfaceIndex = Get-NetIpAddress | Where-Object { $_.IpAddress -like $IpAddress } | Select-Object -ExpandProperty InterfaceIndex
-Set-DnsClientServerAddress -InterfaceIndex $InterfaceIndex -ServerAddresses ($DnsServer)
