a9a508b514c873d82eedfbabdfc1e1dfaa454c87
Victor Fernandez III <48382964+cyberphor@users.noreply.github.com>
2024-01-11 17:05:54 -0500

    Fixed attacker Terraform config

--- platform/attacker/main.tf
+++ platform/attacker/main.tf
@@ -4,33 +4,35 @@ resource "azurerm_marketplace_agreement" "attacker" {
   plan      = "kali-2023-4" # sku
 }
 
-resource "azurerm_virtual_machine" "attacker" {
-  name                  = "attacker"
-  location              = var.location
-  resource_group_name   = var.resource_group_name
-  network_interface_ids = [
-    var.attacker_nic_id
-  ]
-  vm_size               = "Standard_DS1_v2"
-  storage_os_disk {
-    name                = "attacker-os-disk"
-    create_option       = "FromImage"
+resource "azurerm_linux_virtual_machine" "attacker" {
+  name                   = "attacker"
+  location               = var.location
+  resource_group_name    = var.resource_group_name
+  size                   = "Standard_DS1_v2"
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
   }
   plan {
-    publisher           = "kali-linux"
-    product             = "kali"        # offer
-    name                = "kali-2023-4" # sku
+    publisher            = "kali-linux"
+    product              = "kali"        # offer
+    name                 = "kali-2023-4" # sku
   }
-  #os_profile {
-  #  computer_name       = "attacker"
-  #  admin_username      = var.attacker_username
-  #  admin_password      = var.attacker_password
-  #}
-  #os_profile_linux_config {
-  #  disable_password_authentication = false
-  #  #ssh_keys {
-  #  #  path = "~/.ssh/authorized_keys"
-  #  #  key_data = file("~/.ssh/id_rsa.pub")
-  #  #}
-  #}
+  source_image_reference {
+    publisher            = "kali-linux"
+    offer                = "kali"
+    sku                  = "kali-2023-4"
+    version              = "latest"
+  }
+  network_interface_ids  = [
+    var.attacker_nic_id
+  ]
+  computer_name       = "attacker"
+  disable_password_authentication = false
+  admin_username      = var.attacker_username
+  admin_password      = var.attacker_password
+  ssh_keys {
+    path = "~/.ssh/authorized_keys"
+    key_data = file("~/.ssh/id_rsa.pub")
+  }
 }
